eafff730b1e84d2ae9011d5f708f1837d957f0d8
Iskra Krasimirova <82567434+IskraKrasimirova@users.noreply.github.com>
2023-02-06 15:39:31 +0200

    Add files via upload

---
+++ HTML/Task-43/src/js/app.js
@@ -0,0 +1,13 @@
+import "../scss/app.scss";
+
+window.addEventListener("DOMContentLoaded", () => {
+  // This block will be executed once the page is loaded and ready
+
+  const button = document.querySelector(".button");
+  button.addEventListener("click", () => {
+    alert("💣");
+  });
+
+  document.title = 'No new messages';
+  setTimeout(()=> document.title='One new message',3000);
+});
